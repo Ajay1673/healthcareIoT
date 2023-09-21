59195a9bacd230faf99796fa09e06c905550b464
Ajay Rathnam G <ajaynec37@gmail.com>
2023-09-21 12:44:13 +0530

    still template work incomplete

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 
-app = Flask(__name__,static_url_path='/templates', static_folder='templates',template_folder='templates')
+app = Flask(__name__)
 
 # Routes
 @app.get('/')
 def home():
-    return send_from_directory("templates",'home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=80)
+    app.run(debug=True)
+
